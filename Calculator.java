4abf4a9292817c3d06669096075707f3beb984b4
PaulJH88 <PaulJHarder@hotmail.com>
2019-06-20 11:00:44 -0600

    Issue #1 Clean-up
    For the sake of consistency, changed the order of the method call to firstNum, secondNum instead of secondNum, firstNum.

--- Calculator.java
+++ Calculator.java
@@ -73,7 +73,7 @@ public class Calculator {
 				{
 					firstNum = Double.parseDouble(expr.substring(0, plusIndex));
 					secondNum = Double.parseDouble(expr.substring(plusIndex+1));
-					add(secondNum, firstNum);
+					add(firstNum, secondNum);
 				}
 				else if (plusIndex == -1)
 				{
